bd3f50627e2b8eb59b7661867962e5a73dd7f5a8
bczaderna <bczaderna@gmail.com>
2019-04-21 19:06:17 -0400

    still trying to debug productsRoutes. separated orders post routes into own file.

--- server/api/productsRoutes.js
+++ server/api/productsRoutes.js
@@ -1,8 +1,7 @@
 const router = require('express').Router()
-const {Product, Order, } = require('../db/models')
+const {Product} = require('../db/models')
 const db = require('../db')
-//const Product = require('../db/models/product')
-module.exports = router
+
 
 //route to get all products
 router.get('/', async (req, res, next) => {
@@ -31,14 +30,14 @@ router.get('/:id', async (req, res, next) => {
 })
 
 //router to update the inventory in Products db
-router.put('/:id', async (req, res, next) => {
+router.put("/:id", async (req, res, next) => {
  try {
   //how do we know what quantity is being purchased?
   let quantityOrdered = await req.body.quantity
  
   //find product we want to change
   let product = await Product.findByPk(req.params.id)
-​
+
   //figure out how much of it is left
   let currentQuantity = product.dataValues.quantity
 ​
@@ -54,38 +53,5 @@ router.put('/:id', async (req, res, next) => {
  }
 })
 
-//Will need to make a new ordersRoutes.js file in server/api
-
-//we'll need to add a '/checkout' front-end route to display a 'thank you for your order' component
-//this post route will deal with adding info to the Order db once 'checkout' button is clicked.
-router.post('/checkout', async (req, res, next) => {
-  try {
-    if (req.body === "undefined" || req.body === "") {
-      res.sendStatus(500);
-    } else {
-    let newOrder = await Order.create(req.body);
-
-    res.status(200).json(newOrder);
-    }
-  } catch (err) {
-    next(err)
-  }
-})
-
-//this will require access to the itemPurchases db.
-//this post route will deal with adding info to the itemPurchases db once 'checkout' button is clicked.
-router.post('/checkout', async (req, res, next) => {
-  try {
-    if (req.body === "undefined" || req.body === "") {
-      res.sendStatus(500);
-    } else {
-    let newItemPurchase = await ItemPurchases.create(req.body);
-
-    res.status(200).json(newItemPurchase);
-    }
-
-  } catch (err) {
-    next(err)
-  }
-})
 //error handling??
+module.exports = router;
