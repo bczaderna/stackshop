b3c986ca5ccf209775c977c03d300d1e920af2e1
Amy Liao <a.liao.uey@gmail.com>
2019-04-23 17:27:29 -0400

    Flexbox and other CSS updates.

--- client/components/allProducts.js
+++ client/components/allProducts.js
@@ -1,40 +1,39 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import { gettingAllProducts} from '../store/productsReducer'
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {gettingAllProducts} from '../store/productsReducer'
 import ProductCard from './productCard'
 
 class AllProducts extends Component {
-    
-    componentDidMount() {
-        this.props.gettingAllProducts();
-    }
+  componentDidMount() {
+    this.props.gettingAllProducts()
+  }
 
-    render() {
-        const {products} = this.props;
-        //add ternary for if inventory= 0, render ...not found page? / change something in css?
+  render() {
+    const {products} = this.props
+    //add ternary for if inventory= 0, render ...not found page? / change something in css?
 
-        localStorage.setItem('cat', 'meow')
+    localStorage.setItem('cat', 'meow')
 
-        return (
-            <div>
-                {products.map(product => (
-                    <ProductCard key={product.id} product={product}/> 
-                ))}
-            </div>
-        )
-    }
+    return (
+      <div className="allProducts">
+        {products.map(product => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        products: state.products.products
-    }
+  return {
+    products: state.products.products
+  }
 }
 
 const mapDispatchToProps = dispatch => {
-    return {
-        gettingAllProducts: () => dispatch (gettingAllProducts())
-    }
+  return {
+    gettingAllProducts: () => dispatch(gettingAllProducts())
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(AllProducts)
